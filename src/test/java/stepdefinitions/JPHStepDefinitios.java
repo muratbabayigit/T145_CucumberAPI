85ff5add8703b9075d800b33e39468ff72e70f0a
babayigit <babayigit@gmail.com>
2024-12-14 23:24:49 +0300

    cucumber-2.gün

--- src/test/java/stepdefinitions/JPHStepDefinitios.java
+++ src/test/java/stepdefinitions/JPHStepDefinitios.java
@@ -3,9 +3,12 @@ package stepdefinitions;
 import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
 import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
 import io.restassured.path.json.JsonPath;
 import io.restassured.response.Response;
+import org.json.JSONObject;
 import org.junit.jupiter.api.Assertions;
 import utilities.ConfigReader;
 
@@ -14,6 +17,7 @@ public class JPHStepDefinitios {
     String url = "";
     Response actualResponse;
     JsonPath actualResponseJsonPath;
+    JSONObject reqBodyJSON=new JSONObject();
 
     @Given("kullanici {string} adresini kullanir")
     public void kullaniciAdresiniKullanir(String ConfigBaseUrl) {
@@ -58,22 +62,30 @@ public class JPHStepDefinitios {
         Assertions.assertEquals(expValue,actualResponseJsonPath.getString(expKey));
     }
 
-    @Then("kullanici POST request yapabilmek icin {string},{string}{double}{double} degerleri ile reqBody olusturur")
-    public void kullaniciPOSTRequestYapabilmekIcinDegerleriIleReqBodyOlusturur(String arg0, String arg1, int arg2, int arg3) {
-        
+    @Then("kullanici POST request yapabilmek icin {string},{string},{double} {double} degerleri ile reqBody olusturur")
+    public void kullanici_post_request_yapabilmek_icin_degerleri_ile_req_body_olusturur(String title, String body, Double userId, Double id) {
+
+        reqBodyJSON.put("title",title);
+        reqBodyJSON.put("body",body);
+        reqBodyJSON.put("userId",userId);
+        reqBodyJSON.put("id",id);
     }
 
     @Then("kullanici POST request yaparak response degerlerini kaydeder")
     public void kullaniciPOSTRequestYaparakResponseDegerleriniKaydeder() {
-        
+        actualResponse=RestAssured.given().contentType(ContentType.JSON).when().body(reqBodyJSON.toString()).put(url);
     }
 
     @Then("kullanici {string} isimli header degerinin {string} oldugunu kontrol eder")
-    public void kullaniciIsimliHeaderDegerininOldugunuKontrolEder(String arg0, String arg1) {
-        
+    public void kullaniciIsimliHeaderDegerininOldugunuKontrolEder(String headerAttribute, String headerValue) {
+        Assertions.assertEquals(headerValue,actualResponse.getHeader(headerAttribute));
     }
 
     @Then("kullanici userId degerinin {int} oldugunu kontrol eder")
-    public void kullaniciUserIdDegerininOldugunuKontrolEder(int arg0) {
+    public void kullaniciUserIdDegerininOldugunuKontrolEder(int userId) {
+        Assertions.assertEquals(userId,actualResponseJsonPath.getInt("userId"));
+
     }
+
+
 }
--- src/test/resources/features/US002_JPHPostRequest.feature
+++ src/test/resources/features/US002_JPHPostRequest.feature
@@ -1,5 +1,5 @@
 #    https://jsonplaceholder.typicode.com/posts/70 url'ine asagidaki body’e sahip bir
-#    POST request yolladigimizda donen response’in
+#    PUT request yolladigimizda donen response’in
 #    status kodunun 200, content type’inin “application/json; charset=utf-8”,
 #    Connection header degerinin “keep-alive”
 #    ve response body’sinin asagida verilen ile ayni oldugunu test ediniz
@@ -11,18 +11,19 @@
 #    "userId": 10,                          "userId": 10,
 #    "id": 70                               "id": 70
 #   }                                     }
- Feature: US002 POST Request
+ Feature: US002 PUT Request
 
-   Scenario: POST Request yaparak donen responstaki degerlerin
+   Scenario: PUT Request yaparak donen responstaki degerlerin
               beklenen degerlerle karsilastirmasi
 
      Given kullanici "jPHBaseUrl" adresini kullanir
      Then kullanici path paramatresi olarak "posts/70" girer
-     Then kullanici POST request yapabilmek icin "Ahmet","Merhaba",10,70 degerleri ile reqBody olusturur
+     Then kullanici POST request yapabilmek icin "Ahmet","Merhaba",10 70 degerleri ile reqBody olusturur
      Then kullanici POST request yaparak response degerlerini kaydeder
      Then kullanici statusCode degerinin 200 oldugunu kontrol eder
      Then kullanici contentType degerinin "application/json; charset=utf-8" oldugunu kontrol eder
-     Then kullanici "Server" isimli header degerinin "keep-alive" oldugunu kontrol eder
+     Then kullanici "Connection" isimli header degerinin "keep-alive" oldugunu kontrol eder
+     Then kullanici response degerini jsonpath olarak kaydeder
      Then kullanici responsedaki id degerinin 70 oldugunu kontrol eder
      Then kullanici userId degerinin 10 oldugunu kontrol eder
      Then kullanici responsedaki "title" degerinin "Ahmet" oldugunu kontrol eder
